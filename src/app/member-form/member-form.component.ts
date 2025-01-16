d2a70e5322dcf348668c4b26e046fdafe6a33fa3
ahmed <rebai.ahmed@enis.tn>
2025-01-16 13:39:31 +0100

    first commit

--- src/app/member-form/member-form.component.ts
+++ src/app/member-form/member-form.component.ts
@@ -9,77 +9,68 @@ import { MemberService } from 'src/service/member.service';
   styleUrls: ['./member-form.component.css']
 })
 export class MemberFormComponent implements OnInit {
-  form!:FormGroup ;
+  form!: FormGroup;
+  selectedFileName: string = ''; // For displaying the selected file name
 
-  constructor(private ms : MemberService , private router : Router,private activatedRoute :ActivatedRoute){}
-  ngOnInit(){
-    // bch namlo controle : 
-    // premiere recupré id de la route 
+  constructor(
+    private ms: MemberService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
     const idcourant = this.activatedRoute.snapshot.params['id'];
-    console.log(idcourant);
-    // if id exists et a une valeur  ena f edit a
-    //truly  maneha ken fiha valeur
-    if(!!idcourant){
-     this.ms.getMemberById(idcourant).subscribe((member)=>{
-      this.form= new FormGroup ({
-        cin: new FormControl(member.cin,[Validators.required]),
-        name:  new FormControl(member.name,[Validators.required]),
-        cv: new FormControl(member.cv,[]),
-        type: new FormControl(member.type,[Validators.required])
-
-      })
-
-     })
-      
+    if (!!idcourant) {
+      this.ms.getMemberById(idcourant).subscribe((member) => {
+        this.form = new FormGroup({
+          cin: new FormControl(member.cin, [Validators.required]),
+          name: new FormControl(member.name, [Validators.required]),
+          cv: new FormControl(member.cv, [Validators.required]),
+          type: new FormControl(member.type, [Validators.required])
+        });
+      });
+    } else {
+      this.initForm();
     }
-    // sinon ena f create
-    // f create naml init form 
-  else{
-    this.initform() ;
-  }
-}
-
-
-
- initform():void{
-  // creation d'une nouvelle instance de form et initialisation des attributs
-  // n7ebo doub man7elo formulaire ywali fer8
-  this.form= new FormGroup ({
-    cin: new FormControl(null,[Validators.required]),
-    name:  new FormControl(null,[Validators.required]),
-    cv: new FormControl(null,[]),
-    type: new FormControl(null,[Validators.required])
-  })
-}
-
-sub():void{
-  const idcourant = this.activatedRoute.snapshot.params['id'];
-    console.log(idcourant);
-if(!!idcourant){
-  const x= {...this.form.value,createdDate:new Date().toISOString()}
-this.ms.updateMember(x,idcourant).subscribe(()=>{
-  this.router.navigate(['/member'])
-})
-}
-  //recuperer les donnees
-
-  // hna n7eb n7ot les attributs mta form f x o nzidou les attributs ne9sin bch ywalili de type memeber
-  // hethy syntaxe de l'extraction
-  else{
-  const x= {...this.form.value,createdDate:new Date().toISOString()}
-this.ms.add(x).subscribe(()=>{
-  this.router.navigate(['/member'])
-});
-
   }
 
+  initForm(): void {
+    this.form = new FormGroup({
+      cin: new FormControl(null, [Validators.required]),
+      name: new FormControl(null, [Validators.required]),
+      cv: new FormControl(null, [Validators.required]),
+      type: new FormControl(null, [Validators.required])
+    });
+  }
 
+  // File selection handler
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
 
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.selectedFileName = file.name; // Display the file name
+      this.form.patchValue({
+        cv: file.name // Save the file name in the 'cv' field
+      });
+    }
+  }
+
+  sub(): void {
+    const idcourant = this.activatedRoute.snapshot.params['id'];
+    const formData = {
+      ...this.form.value,
+      createdDate: new Date().toISOString()
+    };
+
+    if (!!idcourant) {
+      this.ms.updateMember(formData, idcourant).subscribe(() => {
+        this.router.navigate(['/member']);
+      });
+    } else {
+      this.ms.add(formData).subscribe(() => {
+        this.router.navigate(['/member']);
+      });
+    }
+  }
 }
-
-
-
-
-
-}
-
